d81ceb6afc002b97ac56bf75e3eb3c9de28dafc5
Max Emil Schön <max-emil.schon@icm.uu.se>
2017-06-20 16:06:58 +0200

    fix factorial test

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,13 +1,13 @@
 from swissutil.math import factorial
-
+from pytest import raises
 
 def test_factorial():
-    #there is a significant bug in the factorial function
-    #try calling factorial(-1)
+    # there is a significant bug in the factorial function
+    # try calling factorial(-1)
     assert factorial(0) == 1
     assert factorial(1) == 1
     assert factorial(2) == 2
     assert factorial(3) == 6
     assert factorial(4) == 24
-
-
+    with raises(ValueError):
+        factorial(-1)
